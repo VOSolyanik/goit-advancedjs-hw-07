ccbb77dd38e380342b884ea9fe5780761f27e7c8
VOSolyanik <v.o.solyanik@gmail.com>
2025-01-16 15:46:44 +0200

    Advanced JS HW 07 - init

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,60 @@
+class Key {
+  private signature: string;
+
+  constructor() {
+    // Generate a random string
+    this.signature = Math.random().toString(36).slice(2, 10);
+  }
+
+  getSignature(): string {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.constructor.name} has entered the house.`);
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+    this.door = true;
+    console.log("The door is now open.");
+    } else {
+    console.log("Wrong key. The door remains closed.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
